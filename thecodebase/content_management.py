532185a5df338b7fcfb2141132f5a02a360a6729
Elmeri Niemelä <elmeri.niemela@sprintit.fi>
2019-01-17 11:09:21 +0200

    Added descriptions, improved project layout, added content

--- thecodebase/content_management.py
+++ thecodebase/content_management.py
@@ -1,31 +1,68 @@
 from collections import namedtuple
 
-Project = namedtuple('Project', ['title', 'url', 'html_file'])
-Topic = namedtuple('Topic', ['title', 'url', 'image'])
+Project = namedtuple('Project', ['title', 'url', 'html_file', 'image'])
+Topic = namedtuple('Topic', ['title', 'url', 'image', 'description'])
 
 def Content():
-    return {
-        Topic('Python', url='python', image='python_logo.png'): [
-            Project('Tower Defence', 'TowerDefence', 'tdgame.html'),
-            Project('Bemarifield', 'bem', 'bemarifield.html'),
-            Project('Physics 2D', 'physics', 'physics.html'),
+    python = Topic(
+        title='Python', 
+        url='python', 
+        image='python_logo.png',
+        description="Python is my strongest language. It's the main language I've used professionally."
+    )
 
-            Project('Odoo', 'odoo-portfolio', 'coming-soon.html'),
+    java = Topic(
+        title='Java', 
+        url='java', 
+        image='java_logo.png',
+        description="I learned Java in University of Helsinki. I've also used it in my work at SprintIT.",
+    )
+
+
+    web_development = Topic(
+        title='Web Development', 
+        url='web-development', 
+        image='web-development.jpg',
+        description="This website is an example of my skills with Flask and Bootstrap. I've also learned alot about deploying servers from my work at SprintIT.",
+    )
+    c_c_plus = Topic(
+        title='C/C++', 
+        url='C-C++', 
+        image='c++.png',
+        description="I've studied the basics of C/C++. There's still alot I'd like to do with C++ for example rewrite some of the games I've made with pygame.",
+    )
+    javascript = Topic(
+        title='JavaScript', 
+        url='javascript', 
+        image='javascript.png',
+        description="I aspire to master JavaScript, since I've already encountered it in my work as an Odoo developer, but also in personal projects.",
+    )
+
+    return {
+        python : [
+            Project('Tower Defence', 'TowerDefence', 'tdgame.html', 'tdgame.png'),
+            Project('Bemarifield', 'Bemarifield', 'bemarifield.html', 'bem1.png'),
+            Project('Physics 2D', 'Physics', 'physics.html', 'physics.gif'),
+
+            Project('Odoo', 'odoo-portfolio', 'coming-soon.html', False),
         ],
-        Topic('Java', url='java', image='java_logo.png'): [
-            Project('Tic Tac Toe', 'tic-tac-toe', 'tic-tac-toe.html'),
+        java : [
+            Project('Tic Tac Toe', 'tic-tac-toe', 'tic-tac-toe.html', 'tic-tac-toe.png'),
+            Project('Game Of Life', 'game-of-life', 'coming-soon.html', False),
+            Project('Falling Sand', 'falling-sand', 'coming-soon.html', False),
+            Project('Asteroids', 'asteroids', 'coming-soon.html', False),
         ],
-        Topic('Web Development', url='web-development', image='web-development.jpg'): [
-            Project('Flask', 'flask', 'coming-soon.html'),
-            Project('Django', 'django', 'coming-soon.html'),
+        web_development : [
+            Project('The Codebase', 'thecodebase', 'thecodebase.html', 'server_combined.jpg'),
+            Project('Django', 'django', 'coming-soon.html', False),
         ],
-        Topic('C/C++', url='C-C++', image='c++.png'): [
-            Project('C-practice', 'c-practice', 'c-practice.html'),
-            Project('Python C-extension', 'c-extension', 'coming-soon.html'),
+        c_c_plus : [
+            Project('C-practice', 'c-harjoitus', 'c-practice.html', 'c-practice.png'),
+            Project('Python C-extension', 'c-extension', 'coming-soon.html', False),
         ],
-        Topic('JavaScript', url='javascript', image='javascript.png'): [
-            Project('React.js', 'react-js', 'coming-soon.html'),
-            Project('Bacon.js', 'bacon-js', 'coming-soon.html'),
+        javascript : [
+            Project('React.js', 'react-js', 'coming-soon.html', False),
+            Project('Bacon.js', 'bacon-js', 'coming-soon.html', False),
         ]
     }
     
