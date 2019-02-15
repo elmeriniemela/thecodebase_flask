66c2bd6441197e6c0c32d62c4232a555dd6e9d33
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-02-15 19:16:00 +0200

    preload into separate file

--- thecodebase/static/platform-game/platform-game.js
+++ thecodebase/static/platform-game/platform-game.js
@@ -1,14 +1,3 @@
-function preload ()
-{
-    this.load.image('sky', '/static/platform-game/assets/sky.png');
-    this.load.image('ground', '/static/platform-game/assets/platform.png');
-    this.load.image('star', '/static/platform-game/assets/star.png');
-    this.load.image('bomb', '/static/platform-game/assets/bomb.png');
-    this.load.spritesheet('dude', 
-        '/static/platform-game/assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
-}
 
 var player;
 var stars;
