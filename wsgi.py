8a8b23e0623e6d6bc0777073cf3139e034c3afd5
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-02-19 16:22:09 +0200

    Cleaned wsgi script

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,14 @@
 import sys
 import os
 import logging
-logging.basicConfig(stream=sys.stderr)
-# current_dir = os.path.dirname(os.path.realpath(__file__))
-# sys.path.insert(0, current_dir)
-# sys.path.insert(0, os.path.join(current_dir, 'thecodebase'))
+import json
 
 from thecodebase import app as application
 
-import json
-current_dir = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(current_dir, 'thecodebase', 'credentials.json')) as f:
-    secret_key = json.load(f)["secret_key"]
+logging.basicConfig(stream=sys.stderr)
 
-application.secret_key = secret_key
+DIR = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(DIR, 'thecodebase', 'credentials.json')) as f:
+    KEY = json.load(f)["secret_key"]
+
+application.secret_key = KEY
