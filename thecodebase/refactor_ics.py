ec3f0373a7557cb670ccc44e6bed86e333d56da0
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-03-07 16:04:07 +0200

    Improved refactor algorithm and added page refresh

--- thecodebase/refactor_ics.py
+++ thecodebase/refactor_ics.py
@@ -8,13 +8,13 @@ def auto_refactor(ical_object):
     results = {}
     current_summarys = distinct_summaries(ical_object)
     for summary in current_summarys:
-        parts = sorted(filter(lambda part: bool(part), map(refactor, filter(useful, summary.split(',')))), key=auditorium_last)
-        results.update({summary: ", ".join(parts).strip()})
+        parts = sorted(filter(lambda part: bool(part), map(refactor, summary.split(","))), key=auditorium_last)
+        results.update({summary: refactor(", ".join(parts).strip())})
 
     for key, value in results.items():
-        for component in ical_object.walk(name='VEVENT'):
-            if key == str(component['summary']):
-                component['summary'] = value
+        for component in ical_object.walk(name="VEVENT"):
+            if key == str(component["summary"]):
+                component["summary"] = value
 
     return results
 
@@ -29,50 +29,50 @@ def refactor_file(ics_file):
 
 def distinct_summaries(ical_object):
     summaries = set()
-    for component in ical_object.walk(name='VEVENT'):
-        summaries.add(str(component['summary']))
+    for component in ical_object.walk(name="VEVENT"):
+        summaries.add(str(component["summary"]))
     return summaries
-
-def useful(part):
-    current_year = datetime.now().year
-    exclude = [
-        str(current_year),
-        str(current_year + 1),
-        str(current_year - 1),
-        "Luento",
-        "Lecture",
-        "Föreläsning",
-        "Harjoitukset",
-        "Harjoitus",
-        "Exercises",
-        "Övningar",
-    ]
-    for pattern in exclude:
-        if re.findall(pattern, part):
-            return False
-    return True
 
 def refactor(part):
     replacements = {
-        # Targers cource code ' / 28C00500 - '
+        # Targers cource code " / 28C00500 - "
         r" / .*? - ": " ",
         r"Otakaari 1": "",
         r"Kurssitentti/Course examination/Kurssitentti": "TENTTI",
+        "Välikoe/Midterm exam/Mellanförhör": 'Välikoe',
         r"  ": r" ",
-        r"H\d{2}": r"",
+        r"[HML]\d{2}": r"",
+        r"[^\sA-Öa-ö]*\.+\)": r"",
+        r"\(\d[A-Z0-9]+": r"",
+        r"^\([A-Z0-9-]+\)?": r"",
+        "Pienryhmäopetus": "",
+        "Small group teaching": "",
+        "Pienryhmäopetus": "",
+        "Monimuoto-opetus": "",
+        "Multiform teaching": "",
+        "Monimuoto-opetus": "",
+        "Luento": "",
+        "Lecture": "",
+        "Föreläsning": "",
+        "Harjoitukset": "",
+        "Harjoitus": "",
+        "Exercises": "",
+        "Övningar": "",
+        # Dates separated by [/.-]
+        r"[0-9]{2}[/.-][0-9]{2}[/.-][0-9]{4}": "",
     }
     for find, replace in replacements.items():
         part = re.sub(find, replace, part)
 
-    chars = ', -/'
+    chars = ", -/"
     for _ in range(len(chars)*2):
         part = part.strip(chars)
     return part
 
 def auditorium_last(part):
     auditorium_patterns = [
-        r'^[A-Z]-Sali',
-        r'^[UY][0-9]',
+        r"^[A-Z]-Sali",
+        r"^[UYA][0-9]",
     ]
     for pattern in auditorium_patterns:
         if re.findall(pattern, part):
